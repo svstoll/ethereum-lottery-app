303fc1e0f618957e61916ca3736c1c7da814a6fb
svstoll <nimbatus@posteo.de>
2019-05-22 20:11:02 +0200

    Update lottery SC ABI.

--- src/app/metamask.service.ts
+++ src/app/metamask.service.ts
@@ -222,6 +222,257 @@ export class MetaMaskService {
 
   LOTTERY_ABI: any = [
     {
+      constant: true,
+      inputs: [],
+      name: 'isQueryProcessed',
+      outputs: [
+        {
+          name: '',
+          type: 'bool'
+        }
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: 'isWaitingForWinningNumbers',
+      outputs: [
+        {
+          name: '',
+          type: 'bool'
+        }
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: 'getCurrentRoundStart',
+      outputs: [
+        {
+          name: '',
+          type: 'uint256'
+        }
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: 'isForcedRoundEnd',
+      outputs: [
+        {
+          name: '',
+          type: 'bool'
+        }
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
+      constant: false,
+      inputs: [],
+      name: 'drawWinningNumbers',
+      outputs: [],
+      payable: false,
+      stateMutability: 'nonpayable',
+      type: 'function'
+    },
+    {
+      constant: false,
+      inputs: [],
+      name: 'retrieveLeftOvers',
+      outputs: [],
+      payable: false,
+      stateMutability: 'nonpayable',
+      type: 'function'
+    },
+    {
+      constant: false,
+      inputs: [],
+      name: 'closeLottery',
+      outputs: [],
+      payable: false,
+      stateMutability: 'nonpayable',
+      type: 'function'
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: 'getCurrentRoundEnd',
+      outputs: [
+        {
+          name: '',
+          type: 'uint256'
+        }
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
+      constant: false,
+      inputs: [],
+      name: 'endRound',
+      outputs: [],
+      payable: false,
+      stateMutability: 'nonpayable',
+      type: 'function'
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: 'hasDrawingFinished',
+      outputs: [
+        {
+          name: '',
+          type: 'bool'
+        }
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
+      constant: false,
+      inputs: [],
+      name: 'distributeWinnings',
+      outputs: [],
+      payable: false,
+      stateMutability: 'nonpayable',
+      type: 'function'
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: 'getRoundDuration',
+      outputs: [
+        {
+          name: '',
+          type: 'uint256'
+        }
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: 'getTicketPrice',
+      outputs: [
+        {
+          name: '',
+          type: 'uint256'
+        }
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: 'getJackpot',
+      outputs: [
+        {
+          name: '',
+          type: 'uint256'
+        }
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
+      constant: true,
+      inputs: [
+        {
+          name: '_roundStart',
+          type: 'uint256'
+        }
+      ],
+      name: 'getWinningNumbersForRoundStart',
+      outputs: [
+        {
+          name: '',
+          type: 'string'
+        }
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: 'isLotteryClosed',
+      outputs: [
+        {
+          name: '',
+          type: 'bool'
+        }
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
+      constant: true,
+      inputs: [
+        {
+          name: '_account',
+          type: 'address'
+        }
+      ],
+      name: 'getTicketsForAddress',
+      outputs: [
+        {
+          components: [
+            {
+              name: 'chosenNumbers',
+              type: 'string'
+            },
+            {
+              name: 'roundStart',
+              type: 'uint256'
+            },
+            {
+              name: 'ticketOwner',
+              type: 'address'
+            }
+          ],
+          name: '',
+          type: 'tuple[]'
+        }
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: 'getCurrentParticipants',
+      outputs: [
+        {
+          name: '',
+          type: 'uint256'
+        }
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
       constant: false,
       inputs: [
         {
@@ -230,6 +481,29 @@ export class MetaMaskService {
         }
       ],
       name: 'adjustOracleFee',
+      outputs: [],
+      payable: false,
+      stateMutability: 'nonpayable',
+      type: 'function'
+    },
+    {
+      constant: false,
+      inputs: [
+        {
+          name: '_newOwner',
+          type: 'address'
+        }
+      ],
+      name: 'transferOwnership',
+      outputs: [],
+      payable: false,
+      stateMutability: 'nonpayable',
+      type: 'function'
+    },
+    {
+      constant: false,
+      inputs: [],
+      name: 'distributeRefunds',
       outputs: [],
       payable: false,
       stateMutability: 'nonpayable',
@@ -250,49 +524,24 @@ export class MetaMaskService {
       type: 'function'
     },
     {
-      constant: false,
-      inputs: [],
-      name: 'closeLottery',
-      outputs: [],
-      payable: false,
-      stateMutability: 'nonpayable',
-      type: 'function'
+      inputs: [
+        {
+          name: '_oracleAddress',
+          type: 'address'
+        },
+        {
+          name: '_roundDuration',
+          type: 'uint256'
+        }
+      ],
+      payable: true,
+      stateMutability: 'payable',
+      type: 'constructor'
     },
     {
-      constant: false,
-      inputs: [],
-      name: 'distributeRefunds',
-      outputs: [],
-      payable: false,
-      stateMutability: 'nonpayable',
-      type: 'function'
-    },
-    {
-      constant: false,
-      inputs: [],
-      name: 'distributeWinnings',
-      outputs: [],
-      payable: false,
-      stateMutability: 'nonpayable',
-      type: 'function'
-    },
-    {
-      constant: false,
-      inputs: [],
-      name: 'drawWinningNumbers',
-      outputs: [],
-      payable: false,
-      stateMutability: 'nonpayable',
-      type: 'function'
-    },
-    {
-      constant: false,
-      inputs: [],
-      name: 'endRound',
-      outputs: [],
-      payable: false,
-      stateMutability: 'nonpayable',
-      type: 'function'
+      payable: true,
+      stateMutability: 'payable',
+      type: 'fallback'
     },
     {
       anonymous: false,
@@ -305,15 +554,6 @@ export class MetaMaskService {
       ],
       name: 'LotteryClosedEvent',
       type: 'event'
-    },
-    {
-      constant: false,
-      inputs: [],
-      name: 'retrieveLeftOvers',
-      outputs: [],
-      payable: false,
-      stateMutability: 'nonpayable',
-      type: 'function'
     },
     {
       anonymous: false,
@@ -365,20 +605,6 @@ export class MetaMaskService {
       ],
       name: 'DrawingFinishedEvent',
       type: 'event'
-    },
-    {
-      constant: false,
-      inputs: [
-        {
-          name: '_newOwner',
-          type: 'address'
-        }
-      ],
-      name: 'transferOwnership',
-      outputs: [],
-      payable: false,
-      stateMutability: 'nonpayable',
-      type: 'function'
     },
     {
       anonymous: false,
@@ -447,232 +673,6 @@ export class MetaMaskService {
       ],
       name: 'ForcedRoundEndEvent',
       type: 'event'
-    },
-    {
-      payable: true,
-      stateMutability: 'payable',
-      type: 'fallback'
-    },
-    {
-      inputs: [
-        {
-          name: '_oracleAddress',
-          type: 'address'
-        },
-        {
-          name: '_roundDuration',
-          type: 'uint256'
-        }
-      ],
-      payable: true,
-      stateMutability: 'payable',
-      type: 'constructor'
-    },
-    {
-      constant: true,
-      inputs: [],
-      name: 'getCurrentParticipants',
-      outputs: [
-        {
-          name: '',
-          type: 'uint256'
-        }
-      ],
-      payable: false,
-      stateMutability: 'view',
-      type: 'function'
-    },
-    {
-      constant: true,
-      inputs: [],
-      name: 'getCurrentRoundEnd',
-      outputs: [
-        {
-          name: '',
-          type: 'uint256'
-        }
-      ],
-      payable: false,
-      stateMutability: 'view',
-      type: 'function'
-    },
-    {
-      constant: true,
-      inputs: [],
-      name: 'getCurrentRoundStart',
-      outputs: [
-        {
-          name: '',
-          type: 'uint256'
-        }
-      ],
-      payable: false,
-      stateMutability: 'view',
-      type: 'function'
-    },
-    {
-      constant: true,
-      inputs: [],
-      name: 'getJackpot',
-      outputs: [
-        {
-          name: '',
-          type: 'uint256'
-        }
-      ],
-      payable: false,
-      stateMutability: 'view',
-      type: 'function'
-    },
-    {
-      constant: true,
-      inputs: [],
-      name: 'getRoundDuration',
-      outputs: [
-        {
-          name: '',
-          type: 'uint256'
-        }
-      ],
-      payable: false,
-      stateMutability: 'view',
-      type: 'function'
-    },
-    {
-      constant: true,
-      inputs: [],
-      name: 'getTicketPrice',
-      outputs: [
-        {
-          name: '',
-          type: 'uint256'
-        }
-      ],
-      payable: false,
-      stateMutability: 'view',
-      type: 'function'
-    },
-    {
-      constant: true,
-      inputs: [
-        {
-          name: '_account',
-          type: 'address'
-        }
-      ],
-      name: 'getTicketsForAddress',
-      outputs: [
-        {
-          components: [
-            {
-              name: 'chosenNumbers',
-              type: 'string'
-            },
-            {
-              name: 'roundStart',
-              type: 'uint256'
-            },
-            {
-              name: 'ticketOwner',
-              type: 'address'
-            }
-          ],
-          name: '',
-          type: 'tuple[]'
-        }
-      ],
-      payable: false,
-      stateMutability: 'view',
-      type: 'function'
-    },
-    {
-      constant: true,
-      inputs: [
-        {
-          name: '_roundStart',
-          type: 'uint256'
-        }
-      ],
-      name: 'getWinningNumbersForRoundStart',
-      outputs: [
-        {
-          name: '',
-          type: 'string'
-        }
-      ],
-      payable: false,
-      stateMutability: 'view',
-      type: 'function'
-    },
-    {
-      constant: true,
-      inputs: [],
-      name: 'hasDrawingFinished',
-      outputs: [
-        {
-          name: '',
-          type: 'bool'
-        }
-      ],
-      payable: false,
-      stateMutability: 'view',
-      type: 'function'
-    },
-    {
-      constant: true,
-      inputs: [],
-      name: 'isForcedRoundEnd',
-      outputs: [
-        {
-          name: '',
-          type: 'bool'
-        }
-      ],
-      payable: false,
-      stateMutability: 'view',
-      type: 'function'
-    },
-    {
-      constant: true,
-      inputs: [],
-      name: 'isLotteryClosed',
-      outputs: [
-        {
-          name: '',
-          type: 'bool'
-        }
-      ],
-      payable: false,
-      stateMutability: 'view',
-      type: 'function'
-    },
-    {
-      constant: true,
-      inputs: [],
-      name: 'isQueryProcessed',
-      outputs: [
-        {
-          name: '',
-          type: 'bool'
-        }
-      ],
-      payable: false,
-      stateMutability: 'view',
-      type: 'function'
-    },
-    {
-      constant: true,
-      inputs: [],
-      name: 'isWaitingForWinningNumbers',
-      outputs: [
-        {
-          name: '',
-          type: 'bool'
-        }
-      ],
-      payable: false,
-      stateMutability: 'view',
-      type: 'functcion'
     }
   ];
 
